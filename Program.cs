aeb4560af6847035d66618aa45ecc4bec53a867a
stevenengelman <Stevenengelman@hotmail.nl>
2022-02-06 14:38:06 +0100

    initial commit base project struct

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace traffic_light_simulation
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
